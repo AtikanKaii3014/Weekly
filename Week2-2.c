22ffb78c0b2599fb14fa05ed4a547d3598cffa03
Atikan <88696252+AtikanKaii3014@users.noreply.github.com>
2021-08-12 14:49:03 +0700

    Add files via upload

---
+++ Week2-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a,b,c,max;
+    scanf("%d %d %d",&a, &b ,&c);
+    if(a>=c && b>=c){
+        max = a+b;
+    }else if (a>=b && c>=b)
+    {
+        max = a+c;
+    }else 
+    {
+        max = b+c;
+    }
+    printf("%d",max);
+    
+}
